97c14fd55d6be97c028c6e0c5d7ee8a2b77d0aa1
Anirudh Singh <anirudh.singh@internshala.com>
2023-02-28 16:27:11 +0530

    chagnes

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,37 +15,37 @@ $(window).scroll(function () {
   });
 });
 
-$('.catalogue').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    dots:true,
-    variableWidth: true,
-    arrows: true,
-    infinite: false,
-    // centerMode: true,
-    responsive: [
-      {
-        breakpoint: 1225,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          infinite: false,
-          dots: true,
-          // centerMode: true,
-          arrows: false
-        }
-      },{
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: false,
-          dots: true,
-          centerMode: false,
-          arrows: false
-        }
-      }]
-  });
+// $('.catalogue').slick({
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     dots:true,
+//     variableWidth: true,
+//     arrows: true,
+//     infinite: false,
+//     // centerMode: true,
+//     responsive: [
+//       {
+//         breakpoint: 1225,
+//         settings: {
+//           slidesToShow: 2,
+//           slidesToScroll: 2,
+//           infinite: false,
+//           dots: true,
+//           // centerMode: true,
+//           arrows: false
+//         }
+//       },{
+//         breakpoint: 767,
+//         settings: {
+//           slidesToShow: 1,
+//           slidesToScroll: 1,
+//           infinite: false,
+//           dots: true,
+//           centerMode: false,
+//           arrows: false
+//         }
+//       }]
+//   });
 
 $('.book-now-cta').click(function() {
   window.open("https://wa.me/919871123304?text=Hello%20Annie%27s%20Makeovers%2C%0A%0AI%20would%20like%20to%20request%20a%20booking.");
